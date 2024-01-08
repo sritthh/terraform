e82f3ee7a4f712ce825303b1f8899919f3c3d9e1
sritthh <g3sritth@gmail.com>
2024-01-08 11:59:58 +0530

    hello

---
+++ locals/variables.tf
@@ -0,0 +1,16 @@
+variable "isProd" {
+  type = bool
+  default = false
+}
+
+variable "ami_id" {
+  default = "ami-03265a0778a880afb"
+}
+
+variable "zone_id" {
+  default = "Z07044271ZAND57AHXQLV"
+}
+
+variable "domain_name" {
+  default = "ashritth.online"
+}
